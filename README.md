edade8a91e0e1e3d85fa8ba4d97d44b5b4c4b5a5
Juan Sánchez <jsmontesinos@gmail.com>
2019-12-05 20:50:45 +0100

    Benchmark test project is running correctly

--- README.md
+++ README.md
@@ -3,3 +3,17 @@
 This is a project to test Grafana monitoring utility, with an Nginx and Rabbit-mq.
 
 To test it, a sample project has been included in order to obtain some metrics. The project just get request from nginx, route them through an express app, and this app enqueue these json messages to a queue.
+
+To setup the project benchmark, just run: 
+
+```
+docker-compose up
+```
+
+To check everything is running correctly, launch this from another terminal:
+
+```
+curl -X POST -H "Content-Type: application/json" -d '{"hello": "world"}' http://localhost
+```
+
+
--- node-express/server.js
+++ node-express/server.js
@@ -1,18 +1,53 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+const bodyParser = require("body-parser");
+const amqp = require('amqplib/callback_api');
+
+const queue = 'hello';
+let queueChannel;
+let queueConnection;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', function (req, res) {
   res.send('OK')
 });
 
 app.post('/', function (req, res) {
+  const msg = req.body;
+  console.log("Message received: ", msg);
+  queueChannel.sendToQueue(queue, Buffer.from(JSON.stringify(msg)));
   res.send('OK');
 });
 
 app.listen(5000, function () {
   console.log('Example app listening on port 5000!');
+  setTimeout(() => createQueue(), 20000);
 });
 
 process.on('SIGINT', () => {
+    queueConnection.close();
     process.exit();
 });
+
+function createQueue() {
+  amqp.connect('amqp://rabbitmq', function(error0, connection) {
+    if (error0) {
+      throw error0;
+    }
+    queueConnection = connection;
+    connection.createChannel(function(error1, channel) {
+      if (error1) {
+        throw error1;
+      }
+      
+      channel.assertQueue(queue, {
+        durable: false
+      });
+
+      queueChannel = channel;
+      console.log('Queue started and ready');
+    });
+  });
+}
